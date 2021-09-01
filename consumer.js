edfe2f900ad9cf939c0598539b68c12b33fafba2
duyanhnguyenxuan <duyanh1202tm@gmail.com>
2021-09-01 22:18:17 +0700

    add more lambda

--- consumer.js
+++ consumer.js
@@ -7,3 +7,7 @@ exports.failHandler = async (event) => {
   console.log('--- Signup Fail ---')
   console.log(JSON.stringify(event, null, 2))
 }
+
+exports.hello = async (event) => {
+  console.log('Hello');
+}
